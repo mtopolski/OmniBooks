b06f9b41bf9efb7535dfe9be32212a4183fffeef
Silly Knight <sillyknight1986@gmail.com>
2015-07-31 17:27:34 -0700

    (feat) Added interactive user rating stars

--- public/js/user.js
+++ public/js/user.js
@@ -19,9 +19,55 @@ angular.module('omnibooks.user', ['ui.bootstrap','ngFileUpload','xeditable'])
   if($scope.books.length === 0) {
     noBooks = true;
   }
-}])
-.directive('modal', function() {
-  return {
 
-  };
+  // user rating:
+  $scope.rating = 0;
+  $scope.ratings = [{
+      current: 3,
+      max: 5
+  }];
+
+  $scope.getSelectedRating = function (rating) {
+      // console.log(rating);
+    $scope.ratingValue = rating;
+  }
+}])
+.directive('starRating', function () {
+    return {
+        restrict: 'A',
+        template: '<ul class="rating">' +
+            '<li ng-repeat="star in stars" ng-class="star" ng-click="toggle($index)">' +
+            '\u2605' +
+            '</li>' +
+            '</ul>',
+        scope: {
+            ratingValue: '=',
+            max: '=',
+            onRatingSelected: '&'
+        },
+        link: function (scope, elem, attrs) {
+
+            var updateStars = function () {
+                scope.stars = [];
+                for (var i = 0; i < scope.max; i++) {
+                    scope.stars.push({
+                        filled: i < scope.ratingValue
+                    });
+                }
+            };
+
+            scope.toggle = function (index) {
+                scope.ratingValue = index + 1;
+                scope.onRatingSelected({
+                    rating: index + 1
+                });
+            };
+
+            scope.$watch('ratingValue', function (oldVal, newVal) {
+                if (newVal) {
+                    updateStars();
+                }
+            });
+        }
+    }
 });
